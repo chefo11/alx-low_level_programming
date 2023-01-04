ee9ea1baf21ac87e9eff5e269b2285a99aa3dcf8
Chefo11 <fran6cool@gmail.com>
2023-01-04 10:19:30 -0800

    fixing

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
  * set_string - Sets the value of a pointer to char
  * @init_str: the value to modify
  * @to_new_str: the value to assign as new string
  * Return: Nothing.
  */
-
 void set_string(char **init_str, char *to_new_str)
 {
 	*init_str = to_new_str;
 }
+
